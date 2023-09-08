86f722806ad93db3d0e795a2f91f584bc3c7d15a
Al-Ghani Abdulla <122095905+AlAbdulla@users.noreply.github.com>
2023-09-08 12:37:53 +0800

    MyMVC commit

---
+++ MyMVC/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMVC.Models.EntityManager;
+using MyMVC.Models.ViewModel;
+
+namespace MyMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserModel user)
+        {
+            if(ModelState.IsValid)
+            {
+                UserManager um = new UserManager();
+                if (!um.IsLoginNameExist(user.LoginName))
+                {
+                    um.AddUserAccount(user);
+                    return RedirectToAction("", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetUsers() 
+        {
+            var users = new UserManager().GetAllUsers();
+            return View();
+        }
+    }
+}
---
+++ MyMVC/Models/EntityManager/UserManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Security.Cryptography;
+using MyMVC.Models.DB;
+using MyMVC.Models.ViewModel;
+
+namespace MyMVC.Models.EntityManager
+{
+    public class UserManager
+    {
+        public void AddUserAccount(UserModel user)
+        {
+            using (MyDBContext db = new MyDBContext()) 
+            {
+                SystemUsers newSysUser = new SystemUsers
+                {
+                    LoginName = user.LoginName,
+                    CreatedBy = 1,
+                    PasswordEncryptedText = GetMd5Hash(user.Password),
+                    CreatedDateTime = DateTime.Now,
+                    ModifiedBy = 1,
+                    ModifiedDateTime = DateTime.Now
+                };
+
+                db.SystemUsers.Add(newSysUser);
+                db.SaveChanges();
+
+                int newUserID = db.SystemUsers.First(u => u.LoginName.Equals(newSysUser.LoginName)).UserID;
+
+                Users newUser = new Users
+                {
+                    UserID = newUserID,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Gender = "1",
+                    CreatedBy = 1,
+                    CreatedDateTime = DateTime.Now,
+                    ModifiedBy = 1,
+                    ModifiedDateTime = DateTime.Now
+                };
+
+                db.Users.Add(newUser);
+                db.SaveChanges();
+            }
+        }
+
+        public List<Users> GetAllUsers()
+        {
+            List<Users> users = new List<Users>();
+
+            using (MyDBContext db = new MyDBContext())
+            {
+                users = db.Users.ToList();
+            }
+
+            return users;
+        }
+
+        public bool IsLoginNameExist(string loginName)
+        {
+            using (MyDBContext db = new MyDBContext()) 
+            {
+                return db.SystemUsers.Where(u => u.LoginName.Equals(loginName)).Any();
+            }
+        }
+
+        public string GetMd5Hash(string text)
+        {
+            using MD5 hash = MD5.Create();
+            byte[] data = hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sb.Append(data[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
